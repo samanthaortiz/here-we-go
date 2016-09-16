7938d018e16cc6c4faa9964e5ca631358f8458c4
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-09-16 15:28:33 -0400

    feat( Splash page build):

--- client/components/Splash.js
+++ client/components/Splash.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
-class Splash extends Component {
-
-  render() {
-       return <div><img src='https://images.unsplash.com/photo-1431818563807-927945852ab6?dpr=2&auto=format&crop=entropy&fit=crop&w=1500&h=1125&q=80&cs=tinysrgb' alt="beach" className="img-responsive"/></div>;
-   }
-
+let divStyle = {
+  backgroundImage: 'url(' + 'https://images.unsplash.com/photo-1431818563807-927945852ab6?dpr=2&auto=format&crop=entropy&fit=crop&w=1500&h=1125&q=80&cs=tinysrgb' + ')',
+  height: '100%',
+  width: '100%'
+  // WebkitTransition: 'all', // note the capital 'W' here
+  // msTransition: 'all' // 'ms' is the only lowercase vendor prefix
 }
+
+const Splash = React.createClass({
+
+  getInitialState() {
+    return {value: 'City and State'};
+  },
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  },
+  render() {
+    return (
+      <div style={divStyle}>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button type="submit">Let's Go!</button> 
+      </div>
+    );
+  }
+});
 
 export default Splash
