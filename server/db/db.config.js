2c28120f84795ffd60efd0e69d4621007a86dc3a
Keith Wallace <keith.t.wallace@gmail.com>
2016-09-17 16:44:51 -0400

    style( page): Moved styles.css file
    Moved styles.css file

---
+++ server/db/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dbConfig: {
+    ip: "mysqlcluster14.registeredsite.com",
+    alias: "mysqlcluster14",
+    username: "shadyadmin",
+    password: "!Qaz2wsx3edc",
+    name: "shadycrows"
+  }
+}
