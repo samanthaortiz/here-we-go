d6d58eaa4fb0870e679e53379c1f7f6eecfb274b
Keith Wallace <keith.t.wallace@gmail.com>
2016-09-17 15:39:21 -0400

    feat(): Added HTML wireframe for Dashboard page
    Added HTML wireframe for Dashboard page (Navigation and Dashboard container)

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 class NavigationBar extends Component {
   render() {
     return (
-      <nav>
-        <h1>
-          <a href="/">Here We Go!</a>
-        </h1>
-
-        <div>
-          <a href="/account">My Account</a>
+      <nav className="clearfix">
+        <div className="logo">
+          <h1>
+            <a href="/">
+              Here We Go!
+              </a>
+            </h1>
+        </div>
+        <div className="acct-menu">
+          <ul>
+            <li>f.name l.name</li>
+          </ul>
         </div>
       </nav>
     );
