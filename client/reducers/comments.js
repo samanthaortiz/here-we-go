1cdfb21efe90e95da7548265baed09f773c999c9
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-09-19 14:52:47 -0400

    chore(): Adding reducers

---
+++ client/reducers/comments.js
@@ -0,0 +1,7 @@
+function comments(state = [], action){
+  //the first time this runs, state doesnt equal anything
+  //you need to set it to an empty array
+  return state;
+}
+
+export default comments
