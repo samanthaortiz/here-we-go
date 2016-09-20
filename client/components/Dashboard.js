33b876fa5d84c804c102a5c39aeaf11b79f3a599
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-09-19 20:26:16 -0400

    Working on rendering all data

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -13,6 +13,7 @@ const Dashboard = React.createClass({
         <NavigationBar/>
         <article>
           <div className="dashboard-container">
+<<<<<<< de090097df34a3322123a218234f8e7b3514b587
 <<<<<<< 51e04b6ca11e4be137ec2f436d359e52f30392bd
             {
               //{this.props.expediaInfo.map((info, i) => <Hotels {...this.props} key={i} info={info} />)}
@@ -25,6 +26,9 @@ const Dashboard = React.createClass({
             }
 =======
            {this.props.expediaInfo.map((info, i) => <Hotels {...this.props} key={i} info={info} />)}
+=======
+           {this.props.expediaInfo[0].hotelList.map((info, i) => <Hotels {...this.props} key={i} info={info} />)}
+>>>>>>> Working on rendering all data
 
             DASHBOARD
 >>>>>>> fix(): Expedia info rendering hotel name from hotel list
--- client/components/Hotels.js
+++ client/components/Hotels.js
@@ -2,24 +2,12 @@ import React from 'React';
 
 const Hotel = React.createClass({
   render(){
-<<<<<<< 51e04b6ca11e4be137ec2f436d359e52f30392bd
     var hotel = this.props;
     return (
       <p>
         { this.props.hotelInfo.localizedName } 
       </p>
     )
-=======
-
-    return (
-      <figure className="hotel-figure">
-        <div className="hotel-wrap">
-          { this.props.expediaInfo[0].hotelList[0].name }
-        </div>
-
-      </figure>
-      )
->>>>>>> fix(): Expedia info rendering hotel name from hotel list
   }
 })
 
