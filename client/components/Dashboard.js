4f1345e0507c941ee5525fdde81c1d7d2ab41420
ellier7 <eroussopoulos@gmail.com>
2016-09-20 09:58:20 -0400

    DB Config added to gitignore

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -12,6 +12,7 @@ const Dashboard = React.createClass({
       <div>
         <NavigationBar/>
         <article>
+<<<<<<< 83af176425404f331ff532614ce189b34cef079b
           <div className="dashboard-container clearfix">
             {
               this.props.expediaInfo[0].hotelList.map((hotel) =>
@@ -21,6 +22,14 @@ const Dashboard = React.createClass({
                 />
               )
             }
+=======
+
+
+          <div className="dashboard-container">
+            DASHBOARD
+            {this.props.expediaInfo[0].hotelList.map((hotel) => <Hotels key={hotel.hotelId} hotelInfo={hotel}/>
+              )}
+>>>>>>> DB Config added to gitignore
           </div>
         </article>
       </div> 
