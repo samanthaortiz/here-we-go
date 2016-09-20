e7c247e17fd59e8ee2bbb4439fcf3b02ac661aa1
ellier7 <eroussopoulos@gmail.com>
2016-09-20 11:58:39 -0400

    Merge conflict fixed

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -12,7 +12,6 @@ const Dashboard = React.createClass({
       <div>
         <NavigationBar/>
         <article>
-<<<<<<< 83af176425404f331ff532614ce189b34cef079b
           <div className="dashboard-container clearfix">
             {
               this.props.expediaInfo[0].hotelList.map((hotel) =>
@@ -22,14 +21,6 @@ const Dashboard = React.createClass({
                 />
               )
             }
-=======
-
-
-          <div className="dashboard-container">
-            DASHBOARD
-            {this.props.expediaInfo[0].hotelList.map((hotel) => <Hotels key={hotel.hotelId} hotelInfo={hotel}/>
-              )}
->>>>>>> DB Config added to gitignore
           </div>
         </article>
       </div> 
