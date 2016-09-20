cae681ebe301bdee9fc89e6644ea98e60ced416f
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-09-20 11:29:21 -0400

    style:

--- client/components/Splash.js
+++ client/components/Splash.js
@@ -56,25 +56,23 @@ const Splash = React.createClass({
   render() {
     return (
       <div>
+        <div>
         <h1 id='logo'>
           Here We Go!
-        </h1>    
+        </h1>
+        </div>    
         <div className="splashForm">
-          <div>
+          <div className="input-group" type='text'>
             <input
             type="text"
             placeholder='City and State'
             value={this.state.value}
             onChange={this.handleChangeValue}
-          />
-          </div>
-          <div className="input-group"> 
+            />
             <DatePicker value={this.state.startValue} startDate={this.state.startDate} onChange={this.handleChangeStart}/>
-            <DatePicker value={this.state.endValue} endDate={this.state.endDate} onChange={this.handleChangeEnd}/>
+            <DatePicker value={this.state.endValue} endDate={this.state.endDate} onChange={this.handleChangeEnd}/>    
+            <button className="btn btn-secondary" type="button">Let's Go!</button>
           </div>
-          <span className="input-group-btn">
-          <button className="btn btn-secondary" type="button">Let's Go!</button>
-          </span>
         </div>
       </div>
     );
