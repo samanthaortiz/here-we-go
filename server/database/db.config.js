54ac63b079f3a78438f015162f31ea044234b7a5
Keith Wallace <keith.t.wallace@gmail.com>
2016-09-21 15:55:49 -0400

    chore(TravisCI): Setting Up TravisCI
    Setting up TravisCI for Project

---
+++ server/database/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dbConfig: {
+    ip: "mysqlcluster10.registeredsite.com'",
+    alias: "mysqlcluster10",
+    username: "diamondadmin",
+    password: "!Qaz2wsx3edc",
+    name: "supernovamks"
+  }
+}
