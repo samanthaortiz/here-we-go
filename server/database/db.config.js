33d9fedb0fed7da208b3a242db8b2af566e63c4f
ellier7 <eroussopoulos@gmail.com>
2016-09-22 15:18:58 -0400

    Merge conflicts resolved

--- server/database/db.config.js
+++ server/database/db.config.js
@@ -1,4 +1,3 @@
-<<<<<<< dbaf5414371c9deb4dc2767edd900503bb36fa92
 var dbConfig = {
    ip: "mysqlcluster10.registeredsite.com",
    alias: "mysqlcluster10",
@@ -7,33 +6,4 @@ var dbConfig = {
    name: "supernovamks"
  }
 
-<<<<<<< 735f632d681956bfd4c42e4968bcf8167f58dc68
-<<<<<<< daaaf97ba45558d68a36166d9e03541998b6e730
 module.exports = dbConfig;
-<<<<<<< 5533399cc687aea2909c9263605dcfff1aa289d6
-<<<<<<< efaf08b559c11d274472b3a98de92d131633a180
-=======
-let dbConfig = {
-    ip: "mysqlcluster10.registeredsite.com",
-    alias: "mysqlcluster10",
-    username: "diamondadmin",
-    password: "!Qaz2wsx3edc",
-    name: "supernovamks"
-  }
-
-<<<<<<< 5784f7964b50b4ce9c31868f850e9845d72b2629
-  module.exports = dbConfig;
->>>>>>> Deploying
-=======
-module.exports = dbConfig;
->>>>>>> deployment errors
-=======
->>>>>>> Deploying
-=======
-=======
-module.exports = dbConfig;
->>>>>>> deployment errors
->>>>>>> deployment errors
-=======
-module.exports = dbConfig;
->>>>>>> adding db config edits
