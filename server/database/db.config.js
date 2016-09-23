9ed41a7af4955cb13d31424823c4db758811cec5
Keith Wallace <keith.t.wallace@gmail.com>
2016-09-23 11:16:48 -0400

    chore(Rebase): Fixing rebase conflicts
    Fixing rebase conflicts

--- server/database/db.config.js
+++ server/database/db.config.js
@@ -1,3 +1,4 @@
+<<<<<<< c94164cbef054af6754a38b0f5cc7cfe3aef208d
 var dbConfig = {
    ip: "mysqlcluster10.registeredsite.com",
    alias: "mysqlcluster10",
@@ -5,5 +6,15 @@ var dbConfig = {
    password: "!Qaz2wsx3edc",
    name: "supernovamks"
  }
+=======
+
+let dbConfig = {
+  ip: "mysqlcluster10.registeredsite.com",
+  alias: "mysqlcluster10",
+  username: "diamondadmin",
+  password: "!Qaz2wsx3edc",
+  name: "supernovamks"
+};
+>>>>>>> chore(db.config.js): Added Herkou DB connection settings
 
 module.exports = dbConfig;
