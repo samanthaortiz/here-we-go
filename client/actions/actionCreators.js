894f08ca8582c8ec85859524463cb63e6bf83708
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-09-23 11:41:50 -0400

    feat(): Adding ability to search for flights
    Updated action creators, reducers, store, and added flight components. Need to integrate routing on
    
    backend

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -1,25 +1,21 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 //fire off actions 
-
-//find info?
-
 //actions are just objects
 
 //dispatch with info about the action that happened
 //BUT this does not update our state. 
 //The reducer will do that.
-export const postExpedia = (location, startDate, endDate) => {
+
+
+//=============== HOTELS ===============
+export const postHotelExpedia = (location, startDate, endDate) => {
   console.log(location, startDate, endDate);
   return function(dispatch){
-    return axiosCall(location, startDate, endDate)
+    return axiosHotelCall(location, startDate, endDate)
     .then(res => {
-      // dispatch({
-      //   type: 
-      //   data: response.data.hotelList
-      // })
-      console.log("HERE HERE HERE", res.data);
-      dispatch(hydrateStore(res.data, location, startDate, endDate))
+      console.log("HERE HERE HERE -- HOTELS", res.data);
+      dispatch(hydrateHotelStore(res.data, location, startDate, endDate))
       browserHistory.push('/dashboard')
 
     })
@@ -27,10 +23,10 @@ export const postExpedia = (location, startDate, endDate) => {
   };
 };
 
-export function hydrateStore(expediaInfo, location, startDate, endDate){
+export function hydrateHotelStore(expediaHotelInfo, location, startDate, endDate){
   return {
-    type: 'POST_EXPEDIA',
-    expediaInfo,
+    type: 'POST_HOTEL_EXPEDIA',
+    expediaHotelInfo,
     location,
     startDate,
     endDate
@@ -38,7 +34,7 @@ export function hydrateStore(expediaInfo, location, startDate, endDate){
 }
 
 
-export function axiosCall(location, startDate, endDate){
+export function axiosHotelCall(location, startDate, endDate){
   return axios.post('/api/hotelsearch', {
       location: location,
       startDate: startDate,
@@ -46,42 +42,37 @@ export function axiosCall(location, startDate, endDate){
     });
 }
 
-// export function sendHotelData(list){
-//   return {
-//     type: 'SEND_HOTEL_DATA',
-//     expediaInfo: list
-//   }
-// }
+
+//=============== FLIGHTS ===============
+export const postFlightExpedia = (location, startDate, endDate) => {
+  console.log(location, startDate, endDate);
+  return function(dispatch){
+    return axiosFlightCall(location, startDate, endDate)
+    .then(res => {
+      console.log("HERE HERE HERE -- FLIGHTS", res.data);
+      dispatch(hydrateFlightStore(res.data, location, startDate, endDate))
+      browserHistory.push('/dashboard')
+
+    })
+    .catch(error => console.log(error));
+  };
+};
+
+export function hydrateFlightStore(expediaFlightInfo, location, startDate, endDate){
+  return {
+    type: 'POST_FLIGHT_EXPEDIA',
+    expediaFlightInfo,
+    location,
+    startDate,
+    endDate
+  };
+}
 
 
-// function receiveData(json) {
-//   return{
-//     type: 'RECV_DATA',
-//     data: json
-//   }
-// }
-
-// function receiveError(json) {
-//   return {
-//     type: 'RECV_ERROR',
-//     data: json
-//   }
-// }
-
-// export function fetchData(url) {
-//   return function(dispatch) {
-//     dispatch(postExpedia(location, startDate, endDate));
-//     return axios({
-//       url: url,
-//       method: 'post',
-//       responseType: 'json'
-//     })
-//       .then(function(response) {
-//         dispatch(receiveData(response.data));
-//       })
-//       .catch(function(response){
-//         dispatch(receiveError(response.data));
-//         console.log('error on post req', response.data);
-//       })
-//   }
-// };
+export function axiosFlightCall(location, startDate, endDate){
+  return axios.post('/api/flightSearch', {
+      location: location,
+      startDate: startDate,
+      endDate: endDate
+    });
+}
---
+++ client/components/FlightTile.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Flight = React.createClass({
+  changeDate(date) {
+      let newDate = date.split("-").join("/");
+      let year = newDate.slice(0,4);
+      let monthDay = newDate.slice(5);
+      return monthDay += '/' + year;
+    },
+
+  render(){
+    let flight = this.props;
+
+    return (
+      <div className="tile-flight">
+        <p>
+          <a href={"https://www.expedia.com/New-York-Flights.h"+this.props.flightInfo.flightId+".Flight-Information?rfrr=TG.LP.TopFlights#chkin="+this.changeDate(this.props.startDate)+"&chkout="+this.changeDate(this.props.endDate)} target="_blank">
+          { this.props.flightInfo.localizedName }
+          </a>
+        </p>
+        <p>
+          <img src={'https://images.trvl-media.com' + this.props.flightInfo.largeThumbnailUrl }/>
+        </p>
+        <p>
+          From ${ this.props.flightInfo.lowRateInfo.priceToShowUsers } per night
+        </p>
+        <p>
+          { this.props.flightInfo.shortDescription }
+        </p>
+      </div>
+    )
+  }
+})
+
+export default Flight;
