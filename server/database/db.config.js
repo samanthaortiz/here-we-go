537b70f4b349d6826140fe912e508a961cb23bd5
Keith Wallace <keith.t.wallace@gmail.com>
2016-09-23 11:18:52 -0400

    chore(Rebase): Fixing rebase conflicts
    Fixing rebase conflicts - db.config.js

--- server/database/db.config.js
+++ server/database/db.config.js
@@ -1,9 +1,18 @@
 var dbConfig = {
+<<<<<<< fef4f06596610795f1688804d688253ee32c72cd
    ip: "mysqlcluster10.registeredsite.com",
    alias: "mysqlcluster10",
    username: "diamondadmin",
    password: "!Qaz2wsx3edc",
    name: "supernovamks"
  }
+=======
+  ip: "mysqlcluster10.registeredsite.com",
+  alias: "mysqlcluster10",
+  username: "diamondadmin",
+  password: "!Qaz2wsx3edc",
+  name: "supernovamks"
+};
+>>>>>>> chore(Rebase): Fixing rebase conflicts
 
 module.exports = dbConfig;
