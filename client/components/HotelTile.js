5515600bb6a9d4a56b4422c700baf7a73a416244
Keith Wallace <keith.t.wallace@gmail.com>
2016-09-24 14:16:56 -0400

    feat(Tiles): Handle Empty Data for Tiles
    Handle Empty Data for Tiles

--- client/components/HotelTile.js
+++ client/components/HotelTile.js
@@ -5,23 +5,34 @@ const HotelTile = React.createClass({
   render() {
 
     console.log('>>>>> HOTEL TILE <<<<<');
-    console.log('Hotel: ', this.props);
+    console.log('Hotel: ', this.props.hotelData);
+
+    if(this.props.hotelData.length === 0) {
+      return (
+        <div className="tile-hotel">
+          <Hotel />
+        </div>
+      );
+    } else {
+      return (
+        <div className="tile-hotel">
+          {
+            this.props.hotelData.expediaHotelInfo.hotelList.map((hotel) =>
+              <Hotel
+                key={hotel.hotelId}
+                hotelInfo={hotel}
+                startDate={this.props.hotelData.startDate}
+                endDate={this.props.hotelData.endDate}
+              />
+            )
+          }
+        </div>
+      );
+    }
     
-    return (
-      <div className="tile-hotel">
-        {
-          this.props.hotelData.expediaHotelInfo.hotelList.map((hotel) =>
-            <Hotel
-              key={hotel.hotelId}
-              hotelInfo={hotel}
-              startDate={this.props.hotelData.startDate}
-              endDate={this.props.hotelData.endDate}
-            />
-          )
-        }
-      </div>
-    );
+
   }
+
 });
 
 export default HotelTile;
