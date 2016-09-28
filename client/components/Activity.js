56472352a3a02e1bb726ef061872d170f9b593a6
ellier7 <eroussopoulos@gmail.com>
2016-09-28 19:58:23 -0400

    feat(Activities): Activities tile added
    Activities component set up, api call is made

---
+++ client/components/Activity.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Activity = React.createClass({
+ render() {
+  } 
+});
+
+export default Activity;
---
+++ client/components/ActivityTile.js
@@ -0,0 +1,11 @@
+// DASHBOARD > PANEL > CAR RENTAL TILE ========================================
+import React from 'react';
+import Activity from './Activity';
+
+const ActivityTile = React.createClass({
+render() {
+  console.log('ACTIVITYTILE', this.props)
+}
+});
+
+export default ActivityTile;
