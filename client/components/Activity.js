0e8d8803da5d2226191e47816f9267b06af9a1be
ellier7 <eroussopoulos@gmail.com>
2016-09-30 11:41:37 -0400

    feat(Activity Component): Activity components finished
    Activity components render things to do while on vacation

--- client/components/Activity.js
+++ client/components/Activity.js
@@ -1,14 +1,36 @@
+// DASHBOARD > PANEL > ACTIVITY TILE > ACTIVITY ===========================
 import React from 'react';
+import Hotel from './Hotel';
 
 const Activity = React.createClass({
-render() {
-  	console.log('>>>>ACTIVITY', this.props)
-    return (
+	  changeDate(date) {
+    let newDate = date.split("-").join("/");
+    let year = newDate.slice(0,4);
+    let monthDay = newDate.slice(5);
+    return monthDay += '/' + year;
+  },
+
+  render() {
+	{/*console.log('ACTIVITY', this.props)*/}
+	if(this.props.activityInfo !== undefined) {
+   return (
       <div>
-        <h1>Activities View</h1>
-      </div> 
+        <div>
+      {/*{this.props.activityInfo.categories[0]}<br/>*/}
+        <a href={'https://www.expedia.com/things-to-do/title.a'+ this.props.activityInfo.id + '.activity-details?srp=true&location=' +this.props.location + '&startDate=' + this.changeDate(this.props.startDate) + '&endDate=' + this.changeDate(this.props.endDate)} target="_blank">
+          {this.props.activityInfo.title}</a><br/>
+
+          <img src={this.props.activityInfo.imageUrl} /><br/>
+        </div>
+        <div>
+          {this.props.activityInfo.fromPrice}<br/><br/>
+        </div>
+        </div>
     );
-  }
+}
+  } 
 });
 
 export default Activity;
+
+
