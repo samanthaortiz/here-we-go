c0d34c2c99658b120d7b6e44fd982727ded0463c
ellier7 <eroussopoulos@gmail.com>
2016-10-01 16:36:07 -0400

    Merge conflict

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -99,7 +99,7 @@ router.post('/user-account/', function(req, res) {
 
 router.post("/FlightSearch", function(req, res) {
   // console.log('>> ENTER FLIGHT API ROUTER ', req.body);
-
+  
   var urlAPI =  "http://terminal2.expedia.com:80/x/mflights/search?departureDate="+req.body.startDate+"&returnDate="+req.body.endDate+"&departureAirport="+req.body.departureAirport+"&arrivalAirport="+req.body.destinationAirport+"&prettyPrint=true&numberOfAdultTravelers="+req.body.adults+"&maxOfferCount=20&apikey=OPwVzGiq1hnLYYTDwQI2Uqjt5OPrt767"
 
   request({ url: urlAPI }, function(error, response, body) {
