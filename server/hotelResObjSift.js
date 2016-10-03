bbfff1d561c3c011b92000fb3eba34031426c637
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-10-03 16:05:49 -0400

    chore(deploy):

---
+++ server/hotelResObjSift.js
@@ -0,0 +1,65 @@
+{ username: 'herewegoapp1@gmail.com',
+  domain: 'hotel',
+  gmid: '1546732477416456826',
+  account_id: 'herewegoapp1@gmail.com',
+  email_time: 1475079038,
+  mime_id: '295ca31725eab29d481f3875d4e430b9',
+  host: 'imap.gmail.com',
+  sift_id: 1661579,
+  payload: 
+    { reservationFor: 
+      { 
+        '@type': 'LodgingBusiness',
+        name: 'ME Cancun - Complete ME All Inclusive',
+        telephone: '52 (998) 8812500',
+        'x-days': '4',
+        address: 'Boulevard Kukulkan Km 12, Cancun, QROO, 77500 Mexico' 
+      },
+     reservationStatus: 'http://schema.org/ReservationConfirmed',
+     broker: 
+      { 
+        '@type': 'Organization', 
+        name: 'Expedia' 
+      },
+     checkoutTime: '2014-11-07T00:00:00',
+     reservationId: '185565338793',
+     '@context': 'http://schema.org',
+     checkinTime: '2014-11-03T15:00:00',
+     '@type': 'LodgingReservation' 
+    },
+  mid: '22',
+  fid: '[Gmail]/All Mail' 
+}
+
+
+
+{ username: 'herewegoapp1@gmail.com',
+  domain: 'hotel',
+  gmid: '1546995116422255147',
+  account_id: 'herewegoapp1@gmail.com',
+  email_time: 1475329510,
+  mime_id: 'd51e1dcc9c3528ff04bc276098f4364f',
+  host: 'imap.gmail.com',
+  sift_id: 1661544,
+  payload: 
+   { reservationFor: 
+      { 
+        '@type': 'LodgingBusiness',
+        name: 'La Quinta Inn & Suites Garden City',
+        telephone: '516 - 705-9000',
+        'x-days': '1',
+        address: '821 Stewart Ave Garden City, NY 11530' 
+      },
+     reservationStatus: 'http://schema.org/ReservationConfirmed',
+     broker: { 
+      '@type': 'Organization', name: 'La Quinta' 
+    },
+     checkoutTime: '2015-02-14T12:00:00',
+     reservationId: '3184537495',
+     '@context': 'http://schema.org',
+     checkinTime: '2015-02-13T15:00:00',
+     '@type': 'LodgingReservation' 
+   },
+  mid: '14',
+  fid: '[Gmail]/All Mail' 
+}
