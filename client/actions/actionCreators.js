4ed8cb7f53b66b054bb443303a295f1dcc9c282b
ellier7 <eroussopoulos@gmail.com>
2016-10-04 14:38:16 -0400

    revert(Frontend): Frontend routing for login reversed

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -39,6 +39,7 @@ export function axiosTripCall(location, startDate, endDate){
   });
 };
 
+<<<<<<< 6df3551360344106de446e696515d64f20d8e2dd
 <<<<<<< 2120f9ccaab1fcea4aff7a5e3979b7e6359257f4
 =======
 
@@ -84,6 +85,29 @@ export function axiosTripCall(location, startDate, endDate){
 // }
 
 =======
+=======
+export const login = () => {
+  console.log("You get this far");
+  return function(dispatch) {
+    return axiosLogin()
+    .then(res => {
+      console.log("You don't get to here");
+      dispatch(hydrateLoginStore(res.data))
+      browserHistory.push('/api/auth/google');
+    })
+    .catch(error => console.log(error));
+  };
+};
+
+export function hydrateLoginStore(loginData){
+  return {
+    type: 'GET_LOGIN',
+    loginData
+  };
+}
+
+<<<<<<< 9add38837ff52066af48823227777537884c289a
+>>>>>>> revert(Frontend): Frontend routing for login reversed
 //=============== DATABASE DATA ===============
 
 export const getDatabaseData = () => {
@@ -114,7 +138,52 @@ export function hydrateDBStore(dbData){
 export function axiosLogin(){
   return axios.get('/api/auth/google')
 };
+<<<<<<< 6df3551360344106de446e696515d64f20d8e2dd
 >>>>>>> feat(Database): User email linked to each table
+=======
+=======
+
+// BELOW: TO BE REFACTORED/PURGED
+
+//=============== HOTELS ===============
+// export const postHotelExpedia = (location, startDate, endDate) => {
+
+//   // console.log(location, startDate, endDate);
+//   return function(dispatch){
+//     return axiosHotelCall(location, startDate, endDate)
+//     .then(res => {
+//        // console.log("GET HOTEL", res.data)
+//        console.log('============')
+//        console.log(res.data)
+//        console.log('============')
+//       dispatch(hydrateHotelStore(res.data, location, startDate, endDate))
+//       browserHistory.push('/dashboard')
+
+//     })
+//     .catch(error => console.log(error));
+//   };
+// };
+
+// export function hydrateHotelStore(expediaHotelInfo, location, startDate, endDate){
+//   return {
+//     type: 'POST_HOTEL_EXPEDIA',
+//     expediaHotelInfo,
+//     location,
+//     startDate,
+//     endDate
+//   };
+// }
+
+// export function axiosHotelCall(location, startDate, endDate){
+//   return axios.post('/api/trips', {
+//     location,
+//     startDate,
+//     endDate
+//   });
+// }
+
+>>>>>>> revert(Frontend): Frontend routing for login reversed
+>>>>>>> revert(Frontend): Frontend routing for login reversed
 
 >>>>>>> feat(Database): User email linked to each table
 //=============== FLIGHTS ===============
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,13 +1,22 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import reducerTripData from './reducerTripData';
+<<<<<<< 6df3551360344106de446e696515d64f20d8e2dd
 <<<<<<< d383f6dfe1d18cb426ad8a49abf6e62356b94f8d
+=======
+<<<<<<< 9add38837ff52066af48823227777537884c289a
+>>>>>>> revert(Frontend): Frontend routing for login reversed
 import reducerFlightData from './reducerFlightData'; 
 <<<<<<< 2120f9ccaab1fcea4aff7a5e3979b7e6359257f4
 import reducerLoginData from './reducerLoginData';
+<<<<<<< 6df3551360344106de446e696515d64f20d8e2dd
 import reducerHotelItin from './reducerHotelItin';
 =======
 import reducerDatabaseData from './reducerDatabaseData'; 
+=======
+import reducerFlightData from './reducerFlightData';
+>>>>>>> revert(Frontend): Frontend routing for login reversed
+=======
 =======
 import reducerFlightData from './reducerFlightData';
 >>>>>>> revert(Frontend): Frontend routing for login reversed
@@ -17,11 +26,16 @@ import reducerFlightData from './reducerFlightData';
 // import expediaCarRentalInfo from './expediaCarRentalInfo'; 
 // import expediaActivityInfo from './expediaActivityInfo'; 
 
+<<<<<<< 6df3551360344106de446e696515d64f20d8e2dd
 >>>>>>> feat(Database): User email linked to each table
+=======
+>>>>>>> revert(Frontend): Frontend routing for login reversed
+>>>>>>> revert(Frontend): Frontend routing for login reversed
 
 const rootReducer = combineReducers({
   reducerTripData,
   reducerFlightData,
+<<<<<<< 6df3551360344106de446e696515d64f20d8e2dd
 <<<<<<< d383f6dfe1d18cb426ad8a49abf6e62356b94f8d
 <<<<<<< 2120f9ccaab1fcea4aff7a5e3979b7e6359257f4
   reducerLoginData,
@@ -30,11 +44,20 @@ const rootReducer = combineReducers({
   reducerDatabaseData,
 =======
 >>>>>>> revert(Frontend): Frontend routing for login reversed
+=======
+<<<<<<< 9add38837ff52066af48823227777537884c289a
+  reducerLoginData,
+=======
+>>>>>>> revert(Frontend): Frontend routing for login reversed
   // expediaHotelInfo,
   // expediaFlightInfo,
   // expediaCarRentalInfo,
   // expediaActivityInfo,
+<<<<<<< 6df3551360344106de446e696515d64f20d8e2dd
 >>>>>>> feat(Database): User email linked to each table
+=======
+>>>>>>> revert(Frontend): Frontend routing for login reversed
+>>>>>>> revert(Frontend): Frontend routing for login reversed
   routing: routerReducer
 });
 
