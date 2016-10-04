e4db8282208558316f753b7567fe51925efe15d4
ellier7 <eroussopoulos@gmail.com>
2016-10-04 12:14:23 -0400

    feat(Database): User email linked to each table

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,14 +2,32 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import reducerTripData from './reducerTripData';
 import reducerFlightData from './reducerFlightData'; 
+<<<<<<< 54d2c7812406b7c8acfb0d633bad5c7a2462dd9f
 import reducerLoginData from './reducerLoginData';
 import reducerHotelItin from './reducerHotelItin';
+=======
+import reducerDatabaseData from './reducerDatabaseData'; 
+
+// import expediaHotelInfo from './expediaHotelInfo'; 
+// import expediaFlightInfo from './expediaFlightInfo';
+// import expediaCarRentalInfo from './expediaCarRentalInfo'; 
+// import expediaActivityInfo from './expediaActivityInfo'; 
+
+>>>>>>> feat(Database): User email linked to each table
 
 const rootReducer = combineReducers({
   reducerTripData,
   reducerFlightData,
+<<<<<<< 54d2c7812406b7c8acfb0d633bad5c7a2462dd9f
   reducerLoginData,
   reducerHotelItin,
+=======
+  reducerDatabaseData,
+  // expediaHotelInfo,
+  // expediaFlightInfo,
+  // expediaCarRentalInfo,
+  // expediaActivityInfo,
+>>>>>>> feat(Database): User email linked to each table
   routing: routerReducer
 });
 
