796d949184f11202c044c7040be2f06274ee5c07
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-10-04 19:49:44 -0400

    feat(flight, car, hotel itins): adding to itin component

---
+++ client/reducers/reducerFlightItin.js
@@ -0,0 +1,15 @@
+const reducerFlightItin = (state = [], action) => {
+  //the first time this runs, state doesnt equal anything
+  //you need to set it to an empty array
+  switch (action.type){
+    case 'POST_FLIGHT_ITIN':
+      return {
+        flightItinData: action.flightItinData,
+        email: action.email
+      }
+    default:
+      return state;
+  }
+};
+
+export default reducerFlightItin;
