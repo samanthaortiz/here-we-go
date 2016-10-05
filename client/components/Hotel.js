2c7f00711c561e31ca0795e9c7e1a01a676628af
Keith Wallace <keith.t.wallace@gmail.com>
2016-10-05 10:30:23 -0400

    chore(Hotel Save): Added Save Functionality for Saving Hotel info
    Added save button for hotel and store into database

--- client/components/Hotel.js
+++ client/components/Hotel.js
@@ -3,6 +3,21 @@ import React from 'react';
 
 const Hotel = React.createClass({
 
+  getInitialState() {
+    return {
+      status_id: '',
+      type_id: '',
+      user_email: '',
+      providerName: '',
+      address: '',
+      telephone: '',
+      days: '',
+      startDate: '',
+      endDate: '',
+      organization: ''
+    };
+  },
+
   changeDate(date) {
     let newDate = date.split("-").join("/");
     let year = newDate.slice(0,4);
@@ -10,15 +25,51 @@ const Hotel = React.createClass({
     return monthDay += '/' + year;
   },
 
+  handleSaveHotel: function(event) {
+    event.preventDefault();
+    console.log('>>>>> ', this.props)
+    console.log('>>>>> ', this.props.hotelInfo)
+
+    let dataObj = {
+      status_id: 2,      // Saved
+      type_id: 2,        // hotel
+      user_email: `${this.props.email}`,
+      providerName: `${this.props.hotelInfo.localizedName}`,
+      address: `${this.props.hotelInfo.address}, ${this.props.hotelInfo.city}`,
+      telephone: '',
+      days: '',
+      startDate: `${this.props.startDate}`,
+      endDate: `${this.props.endDate}`,
+      organization: 'Expedia'
+    };
+
+    console.log('>>>> ', dataObj);
+
+    $.ajax({
+      url: '/api/hotelItin',
+      type: 'POST',
+      data: JSON.stringify(dataObj),
+      contentType: 'application/json',
+      success: function (data) {
+        // // Trigger a fetch to update the messages, pass true to animate
+        // app.fetch();
+      },
+      error: function (data) {
+        console.error('ERROR SENDING TO DATABASE: ', data);
+      }
+    });
+  },
+
   render() {
-    
-    // console.log('>>>>> HOTEL <<<<<');
-    // console.log('Hotel: ', this.props.hotelInfo);
+    let saveButton;
+    if (this.props.isLoggedIn) {
+      saveButton = <button type="button" className="btn" onClick={this.handleSaveHotel}>Save Hotel</button>;
+    }
 
     if(this.props.hotelInfo !== undefined) {
       return (
         <div className="item-hotel">
-          <button type="button" className="btn">Save Hotel</button>
+          {saveButton}
           <p>
             <a href={"https://www.expedia.com/New-York-Hotels.h"+this.props.hotelInfo.hotelId+".Hotel-Information?rfrr=TG.LP.TopHotels#chkin="+this.changeDate(this.props.startDate)+"&chkout="+this.changeDate(this.props.endDate)} target="_blank">
             { this.props.hotelInfo.localizedName }
