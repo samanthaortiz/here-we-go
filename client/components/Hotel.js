574578d80d2afe373e4d7ce897cb1ae2f9ec5b70
Keith Wallace <keith.t.wallace@gmail.com>
2016-10-06 19:05:15 -0400

    fix(Itinerary Layout): Fixed CSS Layout issues
    Fixed CSS Layout issues

--- client/components/Hotel.js
+++ client/components/Hotel.js
@@ -66,7 +66,7 @@ const Hotel = React.createClass({
       saveButton = <button type="button" className="btn" onClick={this.handleSaveHotel}>Save Hotel</button>;
     }
 
-    console.log('>>>>>> ', this.props);
+    // console.log('>>>>>> ', this.props);
 
     if(this.props.hotelInfo !== undefined) {
       return (
