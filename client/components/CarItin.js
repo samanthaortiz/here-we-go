87633329272fd7cedb53c978fa7361b49e0b7574
ellier7 <eroussopoulos@gmail.com>
2016-10-06 14:57:00 -0400

    fix(Itinerary fix): Car itinerary dates
    Dates formatted and displayed correctly

--- client/components/CarItin.js
+++ client/components/CarItin.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const CarItin = React.createClass({
+   changeDate(date) {
+    let newDate = date.substring(0,10).split("-").join("/");
+    let year = newDate.slice(0,4);
+    let monthDay = newDate.slice(5);
+    return monthDay += '/' + year;
+  },
 
   render() {
     console.log('rendering car itin', this.props)
@@ -9,6 +15,7 @@ const CarItin = React.createClass({
           <h4>CAR ITIN</h4>
           <p>{this.props.carItinInfo.rentalCompany}</p>
           <p>{this.props.carItinInfo.vehicleBrand}</p>
+          <p>{this.changeDate(this.props.carItinInfo.pickUpTime)} to {this.changeDate(this.props.carItinInfo.dropOffTime)}</p>
         </div>
       );
     } 
