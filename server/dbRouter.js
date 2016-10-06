41659c6d498417166f8b9d9a1ea12642774493f6
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-10-05 20:12:50 -0400

    refactor(routes): refactoring routes for modularization of db calls

---
+++ server/dbRouter.js
@@ -0,0 +1,46 @@
+var router = require("express").Router();
+var db = require('../database/db.js');
+
+// // HOTEL SEARCH API ================================================================== */
+function getHotelData(req, res, next) {
+  // console.log('>> ENTER HOTEL API ROUTER ', req.body);
+
+  var urlAPI = 'http://terminal2.expedia.com:80/x/mhotels/search?city='+req.body.location+'&checkInDate='+req.body.startDate+'&checkOutDate='+req.body.endDate+'&room1=2&apikey=OPwVzGiq1hnLYYTDwQI2Uqjt5OPrt767';
+
+  request({ url: urlAPI }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.data = [];
+      res.data.push({ hotelData: JSON.parse(body) });
+      next();
+    }
+  });
+}
+
+
+
+
+function budgetData(req, res, next) {
+  console.log('>>>>> SAVING BUDGET TO DATABASE: ', req.body);
+  // RAW SQL: INSERT INTO budgets (budgets.type_id, budgets.budget) VALUES 
+  //((SELECT types.id FROM types WHERE types.reservationType = 'hotel'), 100)
+
+  var subSQL;
+  var data = req.body;
+
+  for(var key in data) {
+    if(data.hasOwnProperty(key)) {
+      subSQL = db.knex('types').where('reservationType', key).select('id');
+      db.knex('budgets').insert({budget: data[key], type_id: subSQL})
+      .then(function(user) {
+        // console.log('INSERTED')
+      })
+      .catch(function(error) {
+        console.error(error)
+      });
+    };
+  };
+
+  res.send();
+});
+
+module.exports = budgetData;
