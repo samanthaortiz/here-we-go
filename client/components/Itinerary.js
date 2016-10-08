542ca4eca7ee50cd098b1338c932196547e7fe94
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-10-08 09:09:51 -0400

    feat(itinerary): working on chart in itinerary

--- client/components/Itinerary.js
+++ client/components/Itinerary.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { Accordion, Panel, Modal, Button, ButtonToolbar } from 'react-bootstrap';
 import BudgetForm from './BudgetForm';
+import BudgetChart from './BudgetChart';
 import NewTripForm from './NewTripForm';
 import HotelItin from './HotelItin';
 import FlightItin from './FlightItin';
@@ -285,6 +286,7 @@ const Itinerary = React.createClass({
                 </div>
                 <div id="fade" className="black_overlay"></div>
                 <p><button onClick={this.displayForm}>Add New Budget!</button></p>
+                <BudgetChart/>
             </div>
 
 
