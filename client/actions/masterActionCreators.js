1f99f3e412665f261929c65d9fe1727e7bf0a9b1
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-11-22 13:37:59 -0500

    docs(`):

---
+++ client/actions/masterActionCreators.js
@@ -0,0 +1,25 @@
+import activityItinAction from '../actions/activityItinAction';
+import allTripInfoAction from '../actions/allTripInfoAction';
+import allTripsAction from '../actions/allTripsAction';
+import carItinAction from '../actions/carItinAction';
+import changeStatusAction from '../actions/changeStatusAction';
+import flightDataAction from '../actions/flightDataAction';
+import flightItinAction from '../actions/flightItinAction';
+import hotelItinAction from '../actions/hotelItinAction';
+import newTripAction from '../actions/newTripAction';
+import postTripDataAction from '../actions/postTripDataAction';
+import updateTripAction from '../actions/updateTripAction';
+
+export const ActionCreators = {
+  activityItinAction: activityItinAction,
+  allTripInfoAction: allTripInfoAction,
+  allTripsAction: allTripsAction,
+  carItinAction: carItinAction,
+  changeStatusAction: changeStatusAction,
+  flightDataAction: flightDataAction,
+  flightItinAction: flightItinAction,
+  hotelItinAction: hotelItinAction,
+  newTripAction: newTripAction,
+  postTripDataAction: postTripDataAction,
+  updateTripAction: updateTripAction
+}
